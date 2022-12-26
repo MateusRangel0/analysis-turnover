2e70253edf0653ba643c8c39f264abaf3a3b6a28
MateusRangel0 <mateus.bs.rangel@gmail.com>
2022-12-26 19:07:27 -0300

    Adicionando aspects no processamento

--- src/evaluate_data.py
+++ src/evaluate_data.py
@@ -4,6 +4,27 @@ import generate_dataset as generate_dataset
 generate_dataset.create_columns()
 reviews_train = pd.read_csv('../data/en_training_dataset.csv').astype(str)
 
+absa_model = Sequential()
+absa_model.add(Dense(512, input_shape=(6000,), activation='relu'))
+absa_model.add((Dense(256, activation='relu')))
+absa_model.add((Dense(128, activation='relu')))
+absa_model.add(Dense(9, activation='softmax'))
+# compile model
+absa_model.compile(loss='categorical_crossentropy',
+                optimizer='Adam', metrics=['accuracy'])
+
+vocab_size = 6000  # We set a maximum size for the vocabulary
+tokenizer = Tokenizer(num_words=vocab_size)
+tokenizer.fit_on_texts(reviews_train.review)
+reviews_tokenized = pd.DataFrame(
+tokenizer.texts_to_matrix(reviews_train.review))
+
+label_encoder = LabelEncoder()
+integer_category = label_encoder.fit_transform(
+reviews_train.aspect_category)
+encoded_y = to_categorical(integer_category)
+absa_model.fit(reviews_tokenized, encoded_y, epochs=100, verbose=1)
+
 # model architecture
 sentiment_model = Sequential()
 sentiment_model.add(Dense(512, input_shape=(6000,), activation='relu'))
@@ -32,6 +53,16 @@ test_csv = pd.read_csv('../data/new_en_experiment_dataset.csv').astype(str)
 test_reviews = [review.review.lower() for review in test_csv.itertuples()]
 former_emp = [review.former_emp for review in test_csv.itertuples()]
 
+# Aspect preprocessing
+test_reviews = [review.lower() for review in test_reviews]
+test_aspect_terms = []
+for review in nlp.pipe(test_reviews):
+    chunks = [(chunk.root.text)
+                for chunk in review.noun_chunks if chunk.root.pos_ == 'NOUN']
+    test_aspect_terms.append(' '.join(chunks))
+test_aspect_terms = pd.DataFrame(
+    tokenizer.texts_to_matrix(test_aspect_terms))
+
 # Sentiment preprocessing
 test_sentiment_terms = []
 for review in nlp.pipe(test_reviews):
@@ -44,7 +75,17 @@ test_sentiment_terms = pd.DataFrame(
     tokenizer.texts_to_matrix(test_sentiment_terms))
 
 # Models output
-test_sentiment = label_encoder_2.inverse_transform(sentiment_model.predict_classes(test_sentiment_terms))
+test_aspect_categories = label_encoder.inverse_transform(
+    absa_model.predict_classes(test_aspect_terms))
+test_sentiment = label_encoder_2.inverse_transform(
+    sentiment_model.predict_classes(test_sentiment_terms))
 for i in range(len(test_reviews)-1):
-    print("Review " + str(i+1) + " is expressing a " + test_sentiment[i])
-    generate_dataset.generate_visual_dataset(test_sentiment[i], former_emp[i])
+    print("Review " + str(i+1) + " is expressing a " + test_sentiment[i] + " opinion about " + test_aspect_categories[i])
+    if (test_sentiment[i] == 'positive'):
+        generate_dataset.generate_visual_dataset(3, test_aspect_categories[i], former_emp[i])
+        generate_dataset.generate_quantitative_dataset(3, test_aspect_categories[i], former_emp[i])
+    if (test_sentiment[i] == 'negative'):
+        generate_dataset.generate_visual_dataset(1, test_aspect_categories[i], former_emp[i])
+        generate_dataset.generate_quantitative_dataset(1, test_aspect_categories[i], former_emp[i])
+
+
--- src/generate_dataset.py
+++ src/generate_dataset.py
@@ -1,10 +1,37 @@
 import csv
 
-visual_turnover = open('turnover_sentiment.csv', 'w+', encoding='utf8')
+turnover = open('turnover_sentiment.csv', 'w+', encoding='utf8')
+
+visual_turnover = open('2visual_turnover.csv', 'w+', encoding='utf8')
+
+csv_writer = csv.writer(turnover, delimiter=',', lineterminator='\n')
+
 visual_csv_writer = csv.writer(visual_turnover, delimiter=',', lineterminator='\n')
 
 def create_columns():
-    visual_csv_writer.writerow(["sentiment", "former_emp"])
+    visual_csv_writer.writerow(["sentiment", "aspect", "former_emp"])
+    csv_writer.writerow(["pay", "conditions", "nature_of_work", "promotion", "supervision", "rewards", "coworkers", "communication", "benefits", "former_emp"])
 
-def generate_visual_dataset(sentiment, former_emp):
-    visual_csv_writer.writerow([sentiment, former_emp])
+
+def generate_visual_dataset(sentiment, aspect, former_emp):
+    visual_csv_writer.writerow([sentiment, aspect, former_emp])
+
+def generate_quantitative_dataset(sentiment, aspect, former_emp):
+    if (aspect == 'pay'):
+        csv_writer.writerow([sentiment, 0, 0, 0, 0, 0, 0, 0, 0,  former_emp])
+    if (aspect == 'conditions'):
+        csv_writer.writerow([0, sentiment, 0, 0, 0, 0, 0, 0, 0,  former_emp])
+    if (aspect == 'nature_of_work'):
+        csv_writer.writerow([0, 0, sentiment, 0, 0, 0, 0, 0, 0,  former_emp])
+    if (aspect == 'promotion'):
+        csv_writer.writerow([0, 0, 0, sentiment, 0, 0, 0, 0, 0,  former_emp])
+    if (aspect == 'supervision'):
+        csv_writer.writerow([0, 0, 0, 0, sentiment, 0, 0, 0, 0,  former_emp])
+    if (aspect == 'rewards'):
+        csv_writer.writerow([0, 0, 0, 0, 0, sentiment, 0, 0, 0,  former_emp])
+    if (aspect == 'coworkers'):
+        csv_writer.writerow([0, 0, 0, 0, 0, 0, sentiment, 0, 0,  former_emp])
+    if (aspect == 'communication'):
+        csv_writer.writerow([0, 0, 0, 0, 0, 0, 0, sentiment, 0,  former_emp])
+    if (aspect == 'benefits'):
+        csv_writer.writerow([0, 0, 0, 0, 0, 0, 0, 0, sentiment,  former_emp])
